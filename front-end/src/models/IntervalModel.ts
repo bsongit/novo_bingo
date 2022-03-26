3f0461d4f707685f9ee20030246245e7e90a77d4
besonquadros@gmail.com <besonquadros@gmail.com>
2022-03-26 17:19:48 -0300

    Removendo vite e add ract ts

--- front-end/src/models/IntervalModel.ts
+++
@@ -1,7 +0,0 @@
-export default interface Interval {
-    dataInicial:Date,
-    dataFinal: Date,
-    qtdGerada: number,
-    qtdRestante: number,
-    status: boolean
-  }
--- front-end/src/models/TicketModel.ts
+++
@@ -1,4 +0,0 @@
-export default interface TicketModel {
-    codigo: string,
-    numeros: string
-  }
